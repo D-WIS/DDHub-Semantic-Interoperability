ecd14fce4e4bcbfe52c723d17f82b9d50bb65321
Benoît Daireaux <54928991+benoitdaireaux@users.noreply.github.com>
2021-11-23 10:33:03 +0100

    Started on vocabulary modelling

---
+++ src/DWISVocabularyDevelopment/DWISVocabularyDevelopment/DWISVocabularyDevelopment/Noun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace DWISVocabularyDevelopment
+{
+    public class Noun
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string ParentNounName { get; set; }
+        public Attribute[] Attributes { get; set; }
+        public string Description { get; set; }
+        public string Examples { get; set; }
+        public string DefinitionFileNameWithoutExtension { get; set; }
+
+        public string ToMDSnippet()
+        {
+            return string.Empty;
+        }
+
+        public bool FromMDSnippet()
+        {
+            return false;
+        }
+    
+    
+    }
+
+    public class Attribute
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class SpecializedAttribute
+    {
+        public string AttributeName { get; set; }
+        public string SpecializedValue { get; set; }
+    }
+}
