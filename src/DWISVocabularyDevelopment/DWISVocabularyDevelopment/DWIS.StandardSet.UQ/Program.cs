ed3adcc607e65a157f5fc0360dadd01b39ce5958
Benoît Daireaux <54928991+benoitdaireaux@users.noreply.github.com>
2022-07-29 11:23:06 +0200

    First draft of units and quantities set

--- src/DWISVocabularyDevelopment/DWISVocabularyDevelopment/DWIS.StandardSet.UQ/Program.cs
+++ src/DWISVocabularyDevelopment/DWISVocabularyDevelopment/DWIS.StandardSet.UQ/Program.cs
@@ -2,18 +2,24 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Quantities export");
 string folder = "C:\\Users\\beda\\source\\repos\\D-WIS\\DDHub-Semantic-Interoperability\\docs\\vocabulary_development\\definitions";
-
-var instance = DWIS.StandardSet.UQ.QuantitiesExport.Export(folder);
-//DWIS.Vocabulary.Utils.MDWriting.ToMDFile(instance, )
-foreach (var individual in instance.Population)
+if (VocabularyParsing.FromFolder(folder, out DWISVocabulary vocabulary))
 {
-    Console.WriteLine($"{individual.Name} : {individual.TypeName}");
-    if (individual.Attributes != null)
+    var instance = DWIS.StandardSet.UQ.QuantitiesExport.Export(folder);
+    //DWIS.Vocabulary.Utils.MDWriting.ToMDFile(instance, )
+    foreach (var individual in instance.Population)
     {
-        foreach (var att in individual.Attributes)
+        Console.WriteLine($"{individual.Name} : {individual.TypeName}");
+        if (individual.Attributes != null)
         {
-            Console.WriteLine($"\t{att.AttributeName} = {att.AttributeValue}");
+            foreach (var att in individual.Attributes)
+            {
+                Console.WriteLine($"\t{att.AttributeName} = {att.AttributeValue}");
+            }
         }
     }
+    DWIS.Vocabulary.Utils.MDWriting.ToMDFile(instance, @"C:/temp/uq.md", vocabulary, useLinks:false, addGraph:false);
+    string allText = System.IO.File.ReadAllText(@"C:/temp/uq.md").Replace("- ", "");
+    System.IO.File.WriteAllText(@"C:/temp/uq2.md", allText);
+    Console.WriteLine("Exported instance");
 }
 Console.ReadLine(); 
--- src/DWISVocabularyDevelopment/DWISVocabularyDevelopment/DWIS.StandardSet.UQ/QuantitiesExport.cs
+++ src/DWISVocabularyDevelopment/DWISVocabularyDevelopment/DWIS.StandardSet.UQ/QuantitiesExport.cs
@@ -8,56 +8,93 @@ namespace DWIS.StandardSet.UQ
 {
     public static class QuantitiesExport
     {
+
+        private static string GetValidName(string originalName)
+        {
+            string validName = originalName.Replace(" ", "");
+            string first = validName.Substring(0, 1).ToLower();
+            return first + validName.Substring(1);
+        }
+
         public static DWISInstance Export(string vocabularyFolder)
         {
-            if (Vocabulary.Utils.VocabularyParsing.FromFolder(vocabularyFolder, out DWISVocabulary vocabulary))
+            if (VocabularyParsing.FromFolder(vocabularyFolder, out DWISVocabulary vocabulary))
             {
                 DWISInstance quantities = new DWISInstance("Quantities", vocabulary);
                 quantities.Population = new SimplePopulation();
                 quantities.Sentences = new SimpleSentenceCollection();
+                quantities.ClassAssertions = new SimpleClassAssertionCollection();
 
-                Noun quantityNoun = vocabulary.Nouns.Find(n => n.Name == Nouns.Quantity);
-
-                foreach (var quantity in PhysicalQuantity.AvailableQuantities)
+                if (vocabulary.GetNoun(Nouns.Quantity, out Noun quantityNoun) 
+                    && vocabulary.GetNoun(Nouns.Unit, out Noun unitNoun)
+                    && vocabulary.GetVerb(Verbs.HasSIUnit, out Verb hasSIUnitVerb)
+                    && vocabulary.GetVerb(Verbs.IsUnitForQuantity, out Verb isUnitForQuantityVerb)
+                    )
                 {
-                    TypedIndividual individual = new TypedIndividual(quantity.Name, quantityNoun);
-                    quantities.Population.Add(individual);
+                    foreach (var quantity in PhysicalQuantity.AvailableQuantities)
+                    {
+                        TypedIndividual quantityIndividual = new TypedIndividual(GetValidName(quantity.Name), quantityNoun);
+                        quantities.Population.Add(quantityIndividual);
 
-                    List<Attribute> attributes = new List<Attribute>();
-                    var att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_I);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.ElectricCurrentDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_J);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.LuminousIntensityDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_L);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.LengthDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_M);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.MassDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_N);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.AmountSubstanceDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_T);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.TimeDimension.ToString();
-                    }
-                    att = individual.Attributes.FirstOrDefault(a => a.AttributeName == Attributes.Quantity_ThT);
-                    if (att != null)
-                    {
-                        att.AttributeValue = quantity.TemperatureDimension.ToString();
+                        IndividualAttribute att = null;
+
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_I, out att))
+                        {
+                            att.AttributeValue = quantity.ElectricCurrentDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_J, out att))
+                        {
+                            att.AttributeValue = quantity.LuminousIntensityDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_L, out att))
+                        {
+                            att.AttributeValue = quantity.LengthDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_M, out att))
+                        {
+                            att.AttributeValue = quantity.MassDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_N, out att))
+                        {
+                            att.AttributeValue = quantity.AmountSubstanceDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_T, out att))
+                        {
+                            att.AttributeValue = quantity.TimeDimension.ToString();
+                        }
+                        if (quantityIndividual.GetAttribute(Attributes.Quantity_ThT, out att))
+                        {
+                            att.AttributeValue = quantity.TemperatureDimension.ToString();
+                        }
+
+                        
+
+                        if (quantity.UnitChoices != null)
+                        {
+                            foreach (var unit in quantity.UnitChoices)
+                            {
+                                TypedIndividual unitIndividual = new TypedIndividual(GetValidName(unit.UnitName), unitNoun);
+                                if (unitIndividual.GetAttribute(Attributes.Unit_ConversionFactorA, out att))
+                                {
+                                    att.AttributeValue = unit.ConversionBiasFromSI.ToString();
+                                }
+                                if (unitIndividual.GetAttribute(Attributes.Unit_ConversionFactorB, out att))
+                                {
+                                    att.AttributeValue = unit.ConversionFactorFromSI.ToString();
+                                }
+                                if (unitIndividual.GetAttribute(Attributes.Unit_Symbol, out att))
+                                {
+                                    att.AttributeValue = unit.UnitLabel.ToString();
+                                }
+                                quantities.Population.Add(unitIndividual);
+
+                                if (unit.UnitName == quantity.SIUnitName)
+                                {
+                                    quantities.Sentences.Add(new Sentence(quantityIndividual,hasSIUnitVerb, unitIndividual));
+                                }
+                                quantities.Sentences.Add(new Sentence(unitIndividual, isUnitForQuantityVerb, quantityIndividual));
+                            }
+                        }
                     }
                 }
 
