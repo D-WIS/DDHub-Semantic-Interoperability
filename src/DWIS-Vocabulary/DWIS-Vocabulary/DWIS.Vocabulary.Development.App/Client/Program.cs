51f9a9f92cccdcad0665cc4100251dc520b8194e
Benoît Daireaux <54928991+benoitdaireaux@users.noreply.github.com>
2023-03-30 08:05:16 +0200

    Edition app

--- src/DWIS-Vocabulary/DWIS-Vocabulary/DWIS.Vocabulary.Development.App/Client/Program.cs
+++ src/DWIS-Vocabulary/DWIS-Vocabulary/DWIS.Vocabulary.Development.App/Client/Program.cs
@@ -2,12 +2,14 @@ using DWIS.Vocabulary.Development.App.Client;
 using DWIS.Vocabulary.Development.App.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<IGitHubManager, GitHubManager>();
 builder.Services.AddSingleton<IDiagramManager,DiagramManager>();
 await builder.Build().RunAsync();
